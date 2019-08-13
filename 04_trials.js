10952b345b331ba6017c7c3da5811551a941be46
michael-franke <mchfranke@gmail.com>
2019-08-13 07:34:43 +0300

    updated to new _magpie version

---
+++ 04_trials.js
@@ -0,0 +1,61 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    forced_choice: [
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>The probability that the true mean is greater than 0 is at least 95%.</strong>",
+            item: 1,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>The probability that the true mean equals 0 is smaller than 5%.</strong>",
+            item: 2,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>The “null hypothesis” that the true mean equals 0 is likely to be incorrect.</strong>",
+            item: 3,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>There is a 95 % probability that the true mean lies between 0.1 and 0.4.</strong>",
+            item: 4,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>We can be 95 % confident that the true mean lies between 0.1 and 0.4.</strong>",
+            item: 5,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+        {
+            question: "Is the following statement a valid conclusion to draw from Prof. Bumbledorf's result, given the definition of a 'confidence interval'? <br><br> <strong>If we were to repeat the experiment over and over, then 95 % of the time the true mean falls between 0.1 and 0.4.</strong>",
+            item: 6,
+            QUD: "Professor Bumbledorf conducts an experiment, analyzes the data, and reports:",
+            picture: "images/Prof-Bumbledorf.png",
+            option1: 'yes',
+            option2: 'no',
+            correct: 'no'
+        },
+    ]
+}
