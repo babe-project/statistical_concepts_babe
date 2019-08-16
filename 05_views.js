677bae50a5078be8fdc7ddd40bdea7db48cf1a35
michael-franke <mchfranke@gmail.com>
2019-08-16 08:01:48 +0300

    some minor customizations and tweaks

--- 05_views.js
+++ 05_views.js
@@ -53,26 +53,82 @@ const begin_main = magpieViews.view_generator("begin", {
 
 // In the post test questionnaire you can ask your participants addtional questions
 const post_test = magpieViews.view_generator("post_test", {
-  trials: 1,
-  name: 'post_test',
-  title: 'Additional information',
-  text: 'Answering the following questions is optional, but your answers will help us analyze our results.'
+    trials: 1,
+    name: 'post_test',
+    title: 'Additional information',
+    text: 'Answering the following questions is optional, but your answers will help us analyze our results.',
+},
+{
+        answer_container_generator: function(config, CT) {
+        const quest = magpieUtils.view.fill_defaults_post_test(config);
+        return `<form>
+                    <p class='magpie-view-text'>
+                        <label for="age">${quest.age.title}:</label>
+                        <input type="number" name="age" min="18" max="110" id="age" />
+                    </p>
+                    <p class='magpie-view-text'>
+                        <label for="gender">${quest.gender.title}:</label>
+                        <select id="gender" name="gender">
+                            <option></option>
+                            <option value="${quest.gender.male}">${quest.gender.male}</option>
+                            <option value="${quest.gender.female}">${quest.gender.female}</option>
+                            <option value="${quest.gender.other}">${quest.gender.other}</option>
+                        </select>
+                    </p>
+                    <p class='magpie-view-text'>
+                        <label for="education">${quest.edu.title}:</label>
+                        <select id="education" name="education">
+                            <option></option>
+                            <option value="${quest.edu.graduated_high_school}">${quest.edu.graduated_high_school}</option>
+                            <option value="${quest.edu.graduated_college}">${quest.edu.graduated_college}</option>
+                            <option value="${quest.edu.higher_degree}">${quest.edu.higher_degree}</option>
+                        </select>
+                    </p>
+                    <p class='magpie-view-text'>
+                        <label for="languages" name="languages">${quest.langs.title}:<br /><span>${quest.langs.text}</</span></label>
+                        <input type="text" id="languages"/>
+                    </p>
+                    <p class='magpie-view-text'>
+                        <label for="revisit">Have you taken this survey before?</label>
+                        <select id="revisit" name="revisit">
+                            <option></option>
+                            <option value="yes">yes</option>
+                            <option value="no">no</option>
+                            <option value="maybe">maybe</option>
+                        </select>
+                    </p>
+                    <p class="magpie-view-text">
+                        <label for="comments">${quest.comments.title}</label>
+                        <textarea name="comments" id="comments" rows="6" cols="40"></textarea>
+                    </p>
+                    <button id="next" class='magpie-view-button'>${config.button}</button>
+            </form>`
+        },
+    handle_response_function: function(config, CT, magpie, answer_container_generator, startingTime) {
+        $(".magpie-view").append(answer_container_generator(config, CT));
 
-  // You can change much of what appears here, e.g., to present it in a different language, as follows:
-  // buttonText: 'Weiter',
-  // age_question: 'Alter',
-  // gender_question: 'Geschlecht',
-  // gender_male: 'männlich',
-  // gender_female: 'weiblich',
-  // gender_other: 'divers',
-  // edu_question: 'Höchster Bildungsabschluss',
-  // edu_graduated_high_school: 'Abitur',
-  // edu_graduated_college: 'Hochschulabschluss',
-  // edu_higher_degree: 'Universitärer Abschluss',
-  // languages_question: 'Muttersprache',
-  // languages_more: '(in der Regel die Sprache, die Sie als Kind zu Hause gesprochen haben)',
-  // comments_question: 'Weitere Kommentare'
-});
+        $("#next").on("click", function(e) {
+            // prevents the form from submitting
+            e.preventDefault();
+
+            // records the post test info
+            magpie.global_data.age = $("#age").val();
+            magpie.global_data.gender = $("#gender").val();
+            magpie.global_data.education = $("#education").val();
+            magpie.global_data.languages = $("#languages").val();
+            magpie.global_data.before = $("#revisit").val();
+            magpie.global_data.comments = $("#comments")
+                .val()
+                .trim();
+            magpie.global_data.endTime = Date.now();
+            magpie.global_data.timeSpent =
+                (magpie.global_data.endTime -
+                 magpie.global_data.startTime) /
+                60000;
+
+            // moves to the next view
+            magpie.findNextView();
+        });}});
 
 // The 'thanks' view is crucial; never delete it; it submits the results!
 const thanks = magpieViews.view_generator("thanks", {
@@ -113,10 +169,11 @@ const forced_choice_2A = magpieViews.view_generator("forced_choice", {
   // name should be identical to the variable name
   name: 'forced_choice_2A',
   data: _.shuffle(trial_info.forced_choice),
-  // you can add custom functions at different stages through a view's life cycle
-  // hook: {
-  //     after_response_enabled: check_response
-  // }
+    // you can add custom functions at different stages through a view's life cycle
+  pause: 1000,
+  hook: {
+      after_response_enabled: check_response
+  }
 });
 
 
